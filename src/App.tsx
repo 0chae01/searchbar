7604a0f52f493fc107dd80be7d4cb14a3ecf9d54
youngchae <010101chae@gmail.com>
2023-09-08 12:52:42 +0900

    design: 헤더문구 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { styled } from "styled-components";
 import "./App.css";
 import SearchContainer from "./containers/SearchContainer";
 
 function App() {
   return (
     <div className="App">
+      <Header>
+        국내 모든 임상실험 검색하고 <br /> 온라인으로 참여하기
+      </Header>
       <SearchContainer />
     </div>
   );
 }
 
 export default App;
+
+const Header = styled.header`
+  text-align: center;
+  font-size: 40px;
+  font-weight: 800;
+  line-height: 1.6;
+  margin-top: 80px;
+  margin-bottom: 20px;
+`;
